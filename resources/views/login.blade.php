1ed9fbee5b5d927d312bd3e8528b965a3886eafe
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-11-19 10:47:09 +0100

    patients: model, migration and seeder

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,8 +8,8 @@
         @if($errors->any())
             <p class="alert alert-danger">Die Zugangsdaten sind nicht Korekt.</p>
         @endif
-        <p><input type="text" name="name" placeholder="Benutzer"></p>
-        <p><input type="password" name="password" placeholder="Passwort"></p>
+        <p><input type="text" class="form-control" name="name" placeholder="Benutzer"></p>
+        <p><input type="password" class="form-control" name="password" placeholder="Passwort"></p>
         <p><button type="submit" class="btn btn-primary">Login</button></p>
     </form>
 @endsection
